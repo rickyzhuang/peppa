1f34cebe4fe703fbe0f3195a373bb855bb6f4ef5
jyzhuang <jyzhuang@onlyou.com>
2017-12-26 10:24:32 +0800

    spring boot 启动dubbo服务

--- api/src/main/java/com/peppa/ricky/user/service/IUserService.java
+++ api/src/main/java/com/peppa/ricky/user/service/IUserService.java
@@ -1,5 +1,10 @@
 package com.peppa.ricky.user.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public interface IUserService {
 
 }
